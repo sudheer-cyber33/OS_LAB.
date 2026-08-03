3003228cc2f0a96c911775c2b1eb26f1aa9ca79c
SUDHEER BANDARU <sudheerbandaru65@gmail.com>
2026-08-03 22:06:31 +0530

    Add files via upload
    Added SJF CPU scheduling process

---
+++ WEEK_1_SJF_nonpremitive.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+    int bt[20], wt[20], tat[20], p[20], temp;
+    float avg_wt = 0, avg_tat = 0;
+
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+
+    // Input burst times
+    for(i = 0; i < n; i++) {
+        printf("Enter Burst Time of Process P%d: ", i + 1);
+        scanf("%d", &bt[i]);
+        p[i] = i + 1;
+    }
+
+    // Sort processes according to burst time
+    for(i = 0; i < n - 1; i++) {
+        for(j = i + 1; j < n; j++) {
+            if(bt[i] > bt[j]) {
+                // Swap burst times
+                
+                temp = bt[i];
+                bt[i] = bt[j];
+                bt[j] = temp;
+
+                // Swap process numbers
+                temp = p[i];
+                p[i] = p[j];
+                p[j] = temp;
+            }
+        }
+    }
+
+    // Calculate waiting times
+    wt[0] = 0;
+    for(i = 1; i < n; i++) {
+        wt[i] = wt[i - 1] + bt[i - 1];
+    }
+
+    // Calculate turnaround times
+    for(i = 0; i < n; i++) {
+        tat[i] = wt[i] + bt[i];
+        avg_wt += wt[i];
+        avg_tat += tat[i];
+    }
+
+    // Display results
+    printf("\nProcess\tBurst Time\tWaiting Time\tTurnaround Time\n");
+
+    for(i = 0; i < n; i++) {
+        printf("P%d\t%d\t\t%d\t\t%d\n",
+               p[i], bt[i], wt[i], tat[i]);
+    }
+
+    printf("\nAverage Waiting Time = %.2f", avg_wt / n);
+    printf("\nAverage Turnaround Time = %.2f\n", avg_tat / n);
+
+    return 0;
